ef06f79646f2a093d15176f4c2cca9dd71a6c01f
Pranay <66739544+PranayB003@users.noreply.github.com>
2024-04-08 19:56:02 +0000

    fixed: errors and linting in client code

--- balancedb/src/main/java/site/ycsb/db/HttpClient.java
+++ balancedb/src/main/java/site/ycsb/db/HttpClient.java
@@ -9,81 +9,66 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Class to connect with the BalanceDB instance over HTTP.
+ */
 public class HttpClient {
-    private ArrayList<String> hostnames;
+  private ArrayList<String> hostnames;
 
-    public HttpClient(ArrayList<String> hostnames) {
-        this.hostnames = hostnames;
+  public HttpClient(ArrayList<String> hostnames) {
+    this.hostnames = hostnames;
+  }
+
+  private String getRandomServerUrl() {
+    Random random = new Random();
+    int index = random.nextInt(hostnames.size());
+    return hostnames.get(index);
+  }
+
+  public String sendGetRequest(String data) {
+    String serverUrl = getRandomServerUrl();
+    return sendRequest("GET", serverUrl, data);
+  }
+
+  public String sendPutRequest(String data) {
+    String serverUrl = getRandomServerUrl();
+    return sendRequest("PUT", serverUrl, data);
+  }
+
+  public String sendDeleteRequest(String data) {
+    String serverUrl = getRandomServerUrl();
+    return sendRequest("DELETE", serverUrl, data);
+  }
+
+  private String sendRequest(String method, String serverUrl, String data) {
+    try {
+      URL url = new URL(serverUrl);
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod(method);
+      connection.setDoOutput(true);
+
+      // Send data in request
+      OutputStream outputStream = connection.getOutputStream();
+      outputStream.write(data.getBytes());
+      outputStream.flush();
+      outputStream.close();
+
+      //int responseCode = connection.getResponseCode();
+      //System.out.println(method + " Response Code: " + responseCode);
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      String response;
+      StringBuilder responseData = new StringBuilder();
+      while ((response = reader.readLine()) != null) {
+        responseData.append(response);
+      }
+
+      reader.close();
+      connection.disconnect();
+      return responseData.toString();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "ERROR: HttpClient.sendRequest() failed.";
     }
-
-    private String getRandomServerUrl() {
-        Random random = new Random();
-        int index = random.nextInt(hostnames.size());
-        return hostnames.get(index);
-    }
-
-    public String sendGetRequest(String data) {
-        String serverUrl = getRandomServerUrl();
-        return sendRequest("GET", serverUrl, data);
-    }
-
-    public String sendPutRequest(String data) {
-        String serverUrl = getRandomServerUrl();
-        return sendRequest("PUT", serverUrl, data);
-    }
-
-    public String sendDeleteRequest(String data) {
-        String serverUrl = getRandomServerUrl();
-        return sendRequest("DELETE", serverUrl, data);
-    }
-
-    private String sendRequest(String method, String serverUrl, String data) {
-        try {
-            URL url = new URL(serverUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod(method);
-            connection.setDoOutput(true);
-
-            // Send data in request
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(data.getBytes());
-            outputStream.flush();
-            outputStream.close();
-
-            int responseCode = connection.getResponseCode();
-            System.out.println(method + " Response Code: " + responseCode);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String response;
-            StringBuilder responseData = new StringBuilder();
-            while ((response = reader.readLine()) != null) {
-                responseData.append(response);
-            }
-
-            reader.close();
-            connection.disconnect();
-            return responseData.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-//    public static void main(String[] args) {
-//        String serverUrl = "http://40.80.84.73:8080";
-//
-//        // Send HTTP PUT request
-//        sendPutRequest(serverUrl, "hello=world");
-//        sendPutRequest(serverUrl, "swapnil=tatiya");
-//        sendPutRequest(serverUrl, "amit=pandit");
-//
-//        // Send HTTP GET request
-//        sendGetRequest(serverUrl, "hello");
-//
-//        // Send HTTP DELETE request
-//        sendDeleteRequest(serverUrl, "hue");
-//
-//        // Send HTTP GET request
-//        sendGetRequest(serverUrl, "hello");
-//    }
-
+  }
 }
